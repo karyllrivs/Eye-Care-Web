decca5575af7f5bf188e7342ca4d40fae7d91b4f
karyllrivs <karyllrvr@gmail.com>
2024-10-08 20:34:05 +0800

    delivered to ordered

--- src/pages/OrderHistory.jsx
+++ src/pages/OrderHistory.jsx
@@ -75,7 +75,7 @@ const OrderHistory = () => {
                       scope="col"
                       className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                     >
-                      Delivered On
+                      Ordered On
                     </th>
 
                     <th
