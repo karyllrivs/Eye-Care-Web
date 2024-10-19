ed535a46980dd31b4beef7cc67d81f61b5f4e3f3
karyllrivs <karyllrvr@gmail.com>
2024-10-20 01:39:45 +0800

    lens mng filter

--- src/pages/Personnel/children/LensManagement.jsx
+++ src/pages/Personnel/children/LensManagement.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useRef, useState } from "react"
-import LensConsultationModal from "../components/LensConsultationModal"
+import { useEffect, useRef, useState } from "react";
+import LensConsultationModal from "../components/LensConsultationModal";
 import { MdEventNote } from "react-icons/md";
 import axiosClient from "../../../utils/axiosClient";
 import { formatDateToLong, formatTimeToAMPM } from "../../../utils/formatter";
@@ -9,9 +9,8 @@ import PrintToPDFButton from "../components/PrintToPDFButton";
 import useFilterSearch from "../components/FilterSearch";
 
 const LensManagement = () => {
-
     const [patients, setPatients] = useState([]);
-    const [FilterSearch, filteredData] = useFilterSearch(patients, ["name"]);
+    const [FilterSearch, filteredData] = useFilterSearch(patients, ["name", "email", "mobile", "address"]); // Update fields here
 
     useEffect(() => {
         axiosClient
@@ -19,16 +18,10 @@ const LensManagement = () => {
             .then(({ data }) => {
                 setPatients(getPatientsWithLensConsultation(data));
             })
-            .catch(
-                ({
-                    response: {
-                        data: { message },
-                    },
-                }) => {
-                    alert(message);
-                }
-            );
-    }, [])
+            .catch(({ response: { data: { message } } }) => {
+                alert(message);
+            });
+    }, []);
 
     /** MODAL */
     const [selectedPatient, setSelectedPatient] = useState({});
@@ -45,13 +38,12 @@ const LensManagement = () => {
         setIsModalVisible(false);
     };
 
-
     const divRef = useRef();
     const handlePrint = () => {
         const input = divRef.current;
         const title = "Lens Reports";
         printPage(input, title);
-    }
+    };
 
     return (
         <div className="px-16 py-8">
@@ -65,69 +57,44 @@ const LensManagement = () => {
                 </button>
             </div>
 
-
-            {patients.length == 0 ?
+            {patients.length === 0 ? (
                 <h2 className="text-2xl py-5">No data at the moment.</h2>
-                :
+            ) : (
                 <>
                     <div className="my-10">
                         <div className="flex gap-2">
                             {FilterSearch}
-                         
+
                             <div className="ml-auto">
                                 <PrintToPDFButton handlePrint={handlePrint} />
                             </div>
-                    </div>
+                        </div>
 
                         <div className="relative overflow-x-auto shadow-md sm:rounded-lg" ref={divRef}>
                             <table className="w-full text-sm text-left rtl:text-right text-gray-900 dark:text-gray-900">
                                 <thead className="bg-gray-200">
                                     <tr>
-                                        <th scope="col" className="px-6 py-3">
-                                            Name
-                                        </th>
-                                        <th scope="col" className="px-6 py-3">
-                                            Email
-                                        </th>
-                                        <th scope="col" className="px-6 py-3">
-                                            Mobile
-                                        </th>
-                                        <th scope="col" className="px-6 py-3">
-                                            Address
-                                        </th>
-                                        <th scope="col" className="px-6 py-3">
-                                            Consultation Date
-                                        </th>
-                                        <th scope="col" className="px-6 py-3">
-                                            Consultation Time
-                                        </th>
-                                        <th scope="col" className="px-6 py-3">
-                                            Actions
-                                        </th>
+                                        <th scope="col" className="px-6 py-3">Name</th>
+                                        <th scope="col" className="px-6 py-3">Email</th>
+                                        <th scope="col" className="px-6 py-3">Mobile</th>
+                                        <th scope="col" className="px-6 py-3">Address</th>
+                                        <th scope="col" className="px-6 py-3">Consultation Date</th>
+                                        <th scope="col" className="px-6 py-3">Consultation Time</th>
+                                        <th scope="col" className="px-6 py-3">Actions</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     {filteredData.map((patient, index) => (
-                                        <tr key={index} className="bg-white  dark:border-gray-700">
+                                        <tr key={index} className="bg-white dark:border-gray-700">
                                             <td className="px-6 py-4">
                                                 <MdEventNote className="mr-2 text-xl inline" />
                                                 {patient.name}
                                             </td>
-                                            <td className="px-6 py-4">
-                                                {patient.email}
-                                            </td>
-                                            <td className="px-6 py-4">
-                                                {patient.mobile}
-                                            </td>
-                                            <td className="px-6 py-4">
-                                                {patient.address}
-                                            </td>
-                                            <td className="px-6 py-4">
-                                                {formatDateToLong(patient.lensConsultation.consultationDate)}
-                                            </td>
-                                            <td className="px-6 py-4">
-                                                {formatTimeToAMPM(patient.lensConsultation.consultationTime)}
-                                            </td>
+                                            <td className="px-6 py-4">{patient.email}</td>
+                                            <td className="px-6 py-4">{patient.mobile}</td>
+                                            <td className="px-6 py-4">{patient.address}</td>
+                                            <td className="px-6 py-4">{formatDateToLong(patient.lensConsultation.consultationDate)}</td>
+                                            <td className="px-6 py-4">{formatTimeToAMPM(patient.lensConsultation.consultationTime)}</td>
                                             <td className="px-6 py-4">
                                                 <button
                                                     className="mr-2 bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full"
@@ -143,12 +110,15 @@ const LensManagement = () => {
                         </div>
                     </div>
 
-
-                    <LensConsultationModal handleCloseModal={handleCloseModal} isModalVisible={isModalVisible} selectedPatient={selectedPatient} />
+                    <LensConsultationModal
+                        handleCloseModal={handleCloseModal}
+                        isModalVisible={isModalVisible}
+                        selectedPatient={selectedPatient}
+                    />
                 </>
-            }
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default LensManagement
+export default LensManagement;
